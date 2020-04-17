b0081b4fb7f70ac4ff21f2f632404874af885531
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-04-17 21:36:04 +0200

    initializing project

---
+++ index.js
@@ -0,0 +1,151 @@
+// Librady Code
+
+function createStore(reducer){
+
+  let state
+
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l)=> l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state,action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+
+//App Code
+
+function todos(state = [], action) {
+  switch(action.type){
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' : 
+      return state.filter((todo) => todo.id !== action.todo.id)
+    case 'TOGGLE_TODO' : 
+      return state.map(todo => todo.id !== action.todo.id ? todo : Object.assign({},todo, {complete: !todo.complete}))
+    default :
+      return state
+  }
+}
+
+function goals(state =[], action){
+  switch(action.type){
+    case 'ADD_GOAL' :
+      return state.concat([action.todo])
+    case 'REMOVE_GOAL' : 
+      return state.filter((todo) => todo.id !== action.todo.id)
+    default :
+      return state
+  }
+}
+
+function app(state = {}, action){
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
+const store = createStore(app)
+
+store.subscribe(()=>{
+  console.log('The new state is:', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux 1',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Learn Redux 2',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Learn Redux 3',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 3,
+    name: 'Learn Redux 4',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 4,
+    name: 'Learn Redux 5',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  todo: {
+    id: 2,
+    name: 'Learn Redux',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  todo: {
+    id: 0,
+    name: 'Learn Redux Goal',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  todo: {
+    id: 1,
+    name: 'Learn Redux Goal 2',
+    completed: false
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    completed: false
+  }
+})
+
+
+
